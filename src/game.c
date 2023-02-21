ca6cab228fe26e88cd0106038917e61a660a5f90
Yury Moskov <aerobus.778@gmail.com>
2023-02-21 21:22:43 +0300

    add labels

--- src/game.c
+++ src/game.c
@@ -13,22 +13,25 @@ int game_cycle() {
 	do {
 		// clear();
 
-		wborder(info, 0, 0, 0, 0, 0, 0, 0, 0);
 		mvwprintw(info, 1, 1, "Level: 1");
 		mvwprintw(info, 2, 1, "Exp: 3/8");
 		mvwprintw(info, 3, 1, "Health: 12/24");
+		wborder(info, 0, 0, 0, 0, 0, 0, 0, 0);
+		mvwprintw(info, 0, 1, "Info");
 		wrefresh(info);
 
 		wclear(area);
 		mvwprintw(area, ( LINES - 5 ) / 2 - y, (COLS - 20) / 2 - x, "x");
 		mvwprintw(area, ( LINES - 5 ) / 2, (COLS - 20) / 2, "@");
 		wborder(area, 0, 0, 0, 0, 0, 0, 0, 0);
+		mvwprintw(area, 0, 1, "Area");	
 		wrefresh(area);
 
 		wclear(interactions);
 		mvwprintw(interactions, 1, 1, "(O) open");
 		mvwprintw(interactions, 2, 1, "(T) talk");
 		wborder(interactions, 0, 0, 0, 0, 0, 0, 0, 0);
+		mvwprintw(interactions, 0, 1, "Interactions");
 		wrefresh(interactions);
 
 		// move(LINES / 2 - y, COLS / 2 - x);
